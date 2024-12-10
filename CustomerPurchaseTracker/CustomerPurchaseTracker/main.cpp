6c4291418ccc87a68cad64de0c6960d7cad7335d
Syed Haider <alihaidersyed101@gmail.com>
2024-12-10 18:02:20 -0500

    Add project file

---
+++ CustomerPurchaseTracker/CustomerPurchaseTracker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CustomerPurchaseTracker
+//
+//  Created by Syed12Gen on 12/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
